1e547124a9d618f29af4050403e2c8afe6145f90
Dandee Galang <dandeegalang.ph@gmail.com>
2025-07-16 01:29:29 +0800

    added augments pool

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -3,4 +3,15 @@
 namespace utils
 {
     Camera2D gameCamera = {0};
+
+    std::array<std::array<std::string, 2>, 8> augmentPool = {{
+        {"Rapid Fire", "Increases attack\nspeed by 50%"},
+        {"Trigger Happy", "Boosts fire rate\nfor faster shooting"},
+        {"Scatter Shot", "Fires bullets in\nmultiple directions"},
+        {"Agility Boost", "Increases player\nmovement speed"},
+        {"Iron Core", "Increases max\nhealth capacity"},
+        {"Piercing Rounds", "Bullets pierce through\nenemies"},
+        {"Phase Shift", "Extends iframe duration\nafter damage"},
+        {"Gyro Control", "Increases rotation\nspeed for faster turns"}
+    }};
 };
--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,7 +1,10 @@
 #pragma once
 #include "raylib.h"
+#include <array>
+#include <string>
 
 namespace utils
 {
     extern Camera2D gameCamera;
+    extern std::array<std::array<std::string, 2>, 8> augmentPool;
 };
